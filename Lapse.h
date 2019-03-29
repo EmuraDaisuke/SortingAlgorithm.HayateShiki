bded9f88fb0c76186c03c36d8dd81deea5597c04
dia_maisun <dia_maisun@jcom.home.ne.jp>
2019-03-29 12:00:28 +0900

    .

--- Lapse.h
+++ Lapse.h
@@ -14,9 +14,10 @@ class Lapse final {
     public:
         ~Lapse();
         Lapse();
+        static double Now();
+        static void Out(double Time);
     
     private:
-        double Now();
         double mLapse;
 };
 
@@ -26,7 +27,7 @@ class Lapse final {
 
 Lapse::~Lapse()
 {
-    std::cout << std::fixed << std::setprecision(8) << (Now() - mLapse) << std::endl;
+    Out(Now() - mLapse);
 }
 
 
@@ -43,4 +44,12 @@ Lapse::Now()
 {
     using namespace std::chrono;
     return static_cast<double>(duration_cast<nanoseconds>(steady_clock::now().time_since_epoch()).count()) / 1000000000;
+}
+
+
+
+void
+Lapse::Out(double Time)
+{
+    std::cout << std::fixed << std::setprecision(8) << Time << std::endl;
 }
