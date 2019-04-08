338d08e712222f22f19ca64895425d1aa920c52d
dia_maisun <dia_maisun@jcom.home.ne.jp>
2019-04-09 01:22:14 +0900

    .

--- HayateShiki.h
+++ HayateShiki.h
@@ -66,7 +66,7 @@ struct Dive
 
 // 
 
-inline int Msb(std::size_t v)
+inline int Msb(std::size_t v) noexcept
 {
     if (v){
         union IEEE_754 {
@@ -86,21 +86,21 @@ inline int Msb(std::size_t v)
 
 
 
-inline int MsbAlignment(std::size_t v)
+inline int MsbAlignment(std::size_t v) noexcept
 {
     return Msb(v + v - 1);
 }
 
 
 
-inline int LowerLimit(int v, int limit)
+inline int LowerLimit(int v, int limit) noexcept
 {
     return (v > limit)? v: limit;
 }
 
 
 
-constexpr std::size_t Bit(int v)
+constexpr std::size_t Bit(int v) noexcept
 {
     return (1ULL << v);
 }
@@ -116,25 +116,25 @@ constexpr std::size_t nIns = Bit(bIns);
 
 // 
 
-template <class T> std::size_t Num(T* a, T* e);
+template <class T> std::size_t Num(T* a, T* e) noexcept;
 
 template <class T> T* Copy(T* pDst, T* pSrc, std::size_t nSrc);
 template <class T> T* Copy(T* pDst, T* pSrc, std::size_t nSrc, T& v);
 
-template <class T> T* Join(T* pJoin, Unit<T>* pUnit, Part<T>* pPart);
-template <class T> T* Join(T* aJoin, Unit<T>* pUnit, Part<T>* pPart0, Part<T>* pPart1);
-template <class T> T* Join(T* aJoin, Unit<T>* pUnit, Unit<T>* pUnit0, Unit<T>* pUnit1);
+template <class T> T* Join(T* pJoin, Unit<T>& rUnit, const Part<T>& rPart);
+template <class T> T* Join(T* aJoin, Unit<T>& rUnit, const Part<T>& rPart0, const Part<T>& rPart1);
+template <class T> T* Join(T* aJoin, Unit<T>& rUnit, const Unit<T>& rUnit0, const Unit<T>& rUnit1);
 
-template <class T> T* InitPart(Part<T>* pPart, T* pSrc, T* eSrc, T** paDsc);
+template <class T> T* InitPart(Part<T>& rPart, T* pSrc, T* eSrc, T*& raDsc);
 
-template <class T> void Turn(T* pDst, Part<T>* pPart);
+template <class T> void Turn(T* pDst, const Part<T>& rPart);
 
 
 
 // 
 
 template <class T>
-std::size_t Num(T* a, T* e)
+std::size_t Num(T* a, T* e) noexcept
 {
     return (e - a);
 }
@@ -160,31 +160,31 @@ T* Copy(T* pDst, T* pSrc, std::size_t nSrc, T& v)
 
 
 template <class T>
-T* Join(T* pJoin, Unit<T>* pUnit, Part<T>* pPart)
+T* Join(T* pJoin, Unit<T>& rUnit, const Part<T>& rPart)
 {
-    auto nDsc = pPart->n[Part<T>::oUnit_Dsc];
-    auto nAsc = pPart->n[Part<T>::oUnit_Asc];
+    auto nDsc = rPart.n[Part<T>::oUnit_Dsc];
+    auto nAsc = rPart.n[Part<T>::oUnit_Asc];
     
-    pUnit->a = pJoin;
-    pUnit->n = nDsc + nAsc;
+    rUnit.a = pJoin;
+    rUnit.n = nDsc + nAsc;
     
-    pJoin = Copy(pJoin, pPart->a[Part<T>::oUnit_Dsc], nDsc);
-    pJoin = Copy(pJoin, pPart->a[Part<T>::oUnit_Asc], nAsc);
+    pJoin = Copy(pJoin, rPart.a[Part<T>::oUnit_Dsc], nDsc);
+    pJoin = Copy(pJoin, rPart.a[Part<T>::oUnit_Asc], nAsc);
     return pJoin;
 }
 
 
 
 template <class T>
-T* Join(T* aJoin, Unit<T>* pUnit, Part<T>* pPart0, Part<T>* pPart1)
+T* Join(T* aJoin, Unit<T>& rUnit, const Part<T>& rPart0, const Part<T>& rPart1)
 {
     auto pJoin = aJoin;
-    auto o0 = pPart0->o;
-    auto o1 = pPart1->o;
-    auto p0 = pPart0->a[o0];
-    auto p1 = pPart1->a[o1];
-    auto n0 = pPart0->n[o0];
-    auto n1 = pPart1->n[o1];
+    auto o0 = rPart0.o;
+    auto o1 = rPart1.o;
+    auto p0 = rPart0.a[o0];
+    auto p1 = rPart1.a[o1];
+    auto n0 = rPart0.n[o0];
+    auto n1 = rPart1.n[o1];
     auto v0 = std::move(*p0);
     auto v1 = std::move(*p1);
     
@@ -196,12 +196,12 @@ T* Join(T* aJoin, Unit<T>* pUnit, Part<T>* pPart0, Part<T>* pPart1)
             } else {
                 if (o1){
                     o1 = Part<T>::oUnit_Asc;
-                    p1 = pPart1->a[o1];
-                    n1 = pPart1->n[o1];
+                    p1 = rPart1.a[o1];
+                    n1 = rPart1.n[o1];
                     v1 = std::move(*p1);
                 } else {
                     pJoin = Copy(pJoin, p0, n0, v0);
-                    if (o0) pJoin = Copy(pJoin, pPart0->a[Part<T>::oUnit_Asc], pPart0->n[Part<T>::oUnit_Asc]);
+                    if (o0) pJoin = Copy(pJoin, rPart0.a[Part<T>::oUnit_Asc], rPart0.n[Part<T>::oUnit_Asc]);
                     break;
                 }
             }
@@ -212,33 +212,33 @@ T* Join(T* aJoin, Unit<T>* pUnit, Part<T>* pPart0, Part<T>* pPart1)
             } else {
                 if (o0){
                     o0 = Part<T>::oUnit_Asc;
-                    p0 = pPart0->a[o0];
-                    n0 = pPart0->n[o0];
+                    p0 = rPart0.a[o0];
+                    n0 = rPart0.n[o0];
                     v0 = std::move(*p0);
                 } else {
                     pJoin = Copy(pJoin, p1, n1, v1);
-                    if (o1) pJoin = Copy(pJoin, pPart1->a[Part<T>::oUnit_Asc], pPart1->n[Part<T>::oUnit_Asc]);
+                    if (o1) pJoin = Copy(pJoin, rPart1.a[Part<T>::oUnit_Asc], rPart1.n[Part<T>::oUnit_Asc]);
                     break;
                 }
             }
         }
     }
     
-    pUnit->a = aJoin;
-    pUnit->n = Num(aJoin, pJoin);
+    rUnit.a = aJoin;
+    rUnit.n = Num(aJoin, pJoin);
     return pJoin;
 }
 
 
 
 template <class T>
-T* Join(T* aJoin, Unit<T>* pUnit, Unit<T>* pUnit0, Unit<T>* pUnit1)
+T* Join(T* aJoin, Unit<T>& rUnit, const Unit<T>& rUnit0, const Unit<T>& rUnit1)
 {
     auto pJoin = aJoin;
-    auto p0 = pUnit0->a;
-    auto p1 = pUnit1->a;
-    auto n0 = pUnit0->n;
-    auto n1 = pUnit1->n;
+    auto p0 = rUnit0.a;
+    auto p1 = rUnit1.a;
+    auto n0 = rUnit0.n;
+    auto n1 = rUnit1.n;
     auto v0 = std::move(*p0);
     auto v1 = std::move(*p1);
     
@@ -262,15 +262,15 @@ T* Join(T* aJoin, Unit<T>* pUnit, Unit<T>* pUnit0, Unit<T>* pUnit1)
         }
     }
     
-    pUnit->a = aJoin;
-    pUnit->n = Num(aJoin, pJoin);
+    rUnit.a = aJoin;
+    rUnit.n = Num(aJoin, pJoin);
     return pJoin;
 }
 
 
 
 template <class T>
-T* InitPart(Part<T>* pPart, T* pSrc, T* eSrc, T** paDsc)
+T* InitPart(Part<T>& rPart, T* pSrc, T* eSrc, T*& raDsc)
 {
     auto aAsc = pSrc;
     auto eAsc = aAsc;
@@ -295,7 +295,7 @@ T* InitPart(Part<T>* pPart, T* pSrc, T* eSrc, T** paDsc)
     }
     
     {   // 
-        auto aDsc = *paDsc;
+        auto aDsc = raDsc;
         auto eDsc = aDsc;
         
         auto pOdd = eAsc;
@@ -320,14 +320,14 @@ T* InitPart(Part<T>* pPart, T* pSrc, T* eSrc, T** paDsc)
         
         {   // 
             Auto nDsc = Num(aDsc, eDsc);
-            pPart->a[Part<T>::oUnit_Asc] = aAsc;
-            pPart->n[Part<T>::oUnit_Asc] = Num(aAsc, eAsc);
-            pPart->a[Part<T>::oUnit_Dsc] = aDsc;
-            pPart->n[Part<T>::oUnit_Dsc] = nDsc;
-            pPart->o = (nDsc)? Part<T>::oUnit_Dsc: Part<T>::oUnit_Asc;
+            rPart.a[Part<T>::oUnit_Asc] = aAsc;
+            rPart.n[Part<T>::oUnit_Asc] = Num(aAsc, eAsc);
+            rPart.a[Part<T>::oUnit_Dsc] = aDsc;
+            rPart.n[Part<T>::oUnit_Dsc] = nDsc;
+            rPart.o = (nDsc)? Part<T>::oUnit_Dsc: Part<T>::oUnit_Asc;
         }
         
-        *paDsc = aDsc;
+        raDsc = aDsc;
         return (nOdd)? pOdd: nullptr;
     }
 }
@@ -335,16 +335,16 @@ T* InitPart(Part<T>* pPart, T* pSrc, T* eSrc, T** paDsc)
 
 
 template <class T>
-void Turn(T* pDst, Part<T>* pPart)
+void Turn(T* pDst, const Part<T>& rPart)
 {
-    auto nDsc = pPart->n[Part<T>::oUnit_Dsc];
-    auto nAsc = pPart->n[Part<T>::oUnit_Asc];
+    auto nDsc = rPart.n[Part<T>::oUnit_Dsc];
+    auto nAsc = rPart.n[Part<T>::oUnit_Asc];
     auto aDsc = &pDst[0];
     auto aAsc = &pDst[nDsc];
     
     if (nDsc){
-        Copy(aAsc, pPart->a[Part<T>::oUnit_Asc], nAsc);
-        Copy(aDsc, pPart->a[Part<T>::oUnit_Dsc], nDsc);
+        Copy(aAsc, rPart.a[Part<T>::oUnit_Asc], nAsc);
+        Copy(aDsc, rPart.a[Part<T>::oUnit_Dsc], nDsc);
     }
 }
 }
@@ -385,14 +385,14 @@ void Sort(T* const aSrc, std::size_t nSrc, T* const aExt)
                     {   // 
                         Part<T> vPart0, vPart1;
                         auto aDsc = &aTmp[nSrc];
-                        if ((pSrc = InitPart(&vPart0, pSrc, eSrc, &aDsc))){
-                            pSrc = InitPart(&vPart1, pSrc, eSrc, &aDsc);
-                            pJoin = Join(pJoin, &vUnit, &vPart0, &vPart1);
+                        if ((pSrc = InitPart(vPart0, pSrc, eSrc, aDsc))){
+                            pSrc = InitPart(vPart1, pSrc, eSrc, aDsc);
+                            pJoin = Join(pJoin, vUnit, vPart0, vPart1);
                         } else {
                             if (nJoin){
-                                pJoin = Join(pJoin, &vUnit, &vPart0);
+                                pJoin = Join(pJoin, vUnit, vPart0);
                             } else {
-                                Turn(aSrc, &vPart0);
+                                Turn(aSrc, vPart0);
                                 break;
                             }
                         }
@@ -403,7 +403,7 @@ void Sort(T* const aSrc, std::size_t nSrc, T* const aExt)
                         auto Carry = nJoin++;
                         Carry = (nJoin ^ Carry) & Carry;
                         for (; Carry; Carry >>= 1, ++pDive){
-                            pDive->mpJoin = Join(pDive->mpJoin, &vUnit, &pDive->mUnit, &vUnit);
+                            pDive->mpJoin = Join(pDive->mpJoin, vUnit, pDive->mUnit, vUnit);
                         }
                         pDive->mUnit = vUnit;
                     }
@@ -421,7 +421,7 @@ void Sort(T* const aSrc, std::size_t nSrc, T* const aExt)
                     Carry >>= oDive;
                     for (; Carry; Carry >>= 1, ++pDive){
                         if (Carry & Bit(0)){
-                            Join(pDive->mpJoin, &pResult->mUnit, &pDive->mUnit, &pResult->mUnit);
+                            Join(pDive->mpJoin, pResult->mUnit, pDive->mUnit, pResult->mUnit);
                         }
                     }
                 }
